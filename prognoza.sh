b979e98d7a84808c60e92192bc194da2cefa7586
marcinprzybysz86 <44779944+marcinprzybysz86@users.noreply.github.com>
2019-11-25 17:42:51 +0100

    Update prognoza.sh

--- prognoza.sh
+++ prognoza.sh
@@ -11,16 +11,13 @@ LOC="warszawa,pl"
 IDX=<domoticz_dummy_temp_idx>
 DOMO="http://127.0.0.1:8080"
 
-curl "api.openweathermap.org/data/2.5/forecast?q=$LOC&appid=$APIKEY" -o pogoda.json
-TIME=`cat pogoda.json | jq ".list[5].dt_txt"`
-TEMP=`cat pogoda.json | jq ".list[5].main.temp"`
-### free api lets us get forecasted values for the next 3,6,9,12,15 ... hours up to 5 days. 
-### In above lines you can change list[5] to list [0] to get forecast for 3hrs. You mauy change it to list[7] to get forecast for 24hrs from now.
-
+RESULT=`curl "api.openweathermap.org/data/2.5/forecast?q=$LOC&appid=$APIKEY"`
+TIME=`echo $RESULT | jq ".list[5].dt_txt"`
+TEMP=`echo $RESULT | jq ".list[5].main.temp"`
 TEMPINT=${TEMP%.*}
 (( TEMPC="$TEMPINT-273" ))
 echo $TIME
 echo $TEMPC
 curl --silent "$DOMO/json.htm?type=command&param=udevice&idx=$IDX&nvalue=0&svalue=$TEMPC"
-rm -f pogoda.json
+
 
